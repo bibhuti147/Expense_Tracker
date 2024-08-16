ed4d6b2632852867f4668cdf6ca66475cc59e853
bibhuti <bibhutinayak309@gmail.com>
2024-08-17 03:34:43 +0530

    7th Commit

--- app/(routes)/dashboard/budgets/page.jsx
+++ app/(routes)/dashboard/budgets/page.jsx
@@ -10,7 +10,7 @@ const Budget = () => {
     <div className="p-10">
       <h2 className="font-bold text-3xl flex gap-2 items-center">
         <ArrowLeft
-          onClick={() => route.replace("./dashboard")}
+          onClick={() => route.replace("/dashboard")}
           className="cursor-pointer"
         />{" "}
         My Budgets
